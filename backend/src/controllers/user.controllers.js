ba11778814166452774d5c45788740bb7214f460
franciscode11 <vallesfrancisco2025@gmail.com>
2025-03-15 16:06:06 -0300

    fix the sameSite options for cookies

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -71,7 +71,7 @@ const login = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
